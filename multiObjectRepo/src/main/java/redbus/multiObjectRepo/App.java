2f385ab4f30c40ce639a45600ac2044a3a3568e8
nivassahoo <30389541+nivassahoo@users.noreply.github.com>
2019-02-05 19:23:25 +0530

    Update App.java

--- multiObjectRepo/src/main/java/redbus/multiObjectRepo/App.java
+++ multiObjectRepo/src/main/java/redbus/multiObjectRepo/App.java
@@ -10,4 +10,44 @@ public class App
     {
         System.out.println( "Hello World! checking fetch again" );
     }
+    
+    public void swipeBasedOnText(String strText) throws Throwable {
+		try {
+			if (DriverManagerFactory.getDriverData("Platform Name").trim().equalsIgnoreCase("ANDROID")) {
+				appiumDriver.findElement(MobileBy.AndroidUIAutomator(
+						"new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textMatches(\""
+								+ strText + "\").instance(0))"));
+				logger.info("scrolled to the element with text :: " + strText);
+			} else if (DriverManagerFactory.getDriverData("Platform Name").trim().equalsIgnoreCase("IOS")) {
+				appiumDriver.findElement(MobileBy.IosUIAutomation(
+						"new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textMatches(\""
+								+ strText + "\").instance(0))"));
+				logger.info("scrolled to the element with text :: " + strText);
+			}
+		} catch (Throwable t) {
+			logger.error(
+					"Error while scrolling to the element with text :: " + strText + " " + t.getLocalizedMessage());
+			Config.gracefulEnd(t, logger);
+		}
+	}
+    
+    
+    protected boolean Swipe(int startHeightFrom, int startWidthFrom, int endHeightAt, int entWidthAt) {
+		try {
+			// mobileDriver.swipe(X_AXIS / startWidthFrom, Y_AXIS / startHeightFrom, X_AXIS
+			// / entWidthAt,Y_AXIS / endHeightAt, 750);
+
+			dim = getScreenResolution();
+			new TouchAction(mobileDriver)
+					.press(PointOption.point(dim.getWidth() / startWidthFrom, dim.getHeight() / startHeightFrom))
+					.waitAction(WaitOptions.waitOptions(Duration.ofMillis(750)))
+					.moveTo(PointOption.point(dim.getWidth() / entWidthAt, dim.getHeight() / endHeightAt)).release()
+					.perform();
+		} catch (Throwable t) {
+			Config.gracefulEnd(t, logger);
+			logger.error("Returning 'false', Error while swiping :: " + t.getLocalizedMessage());
+			return false;
+		}
+		return true;
+	}
 }
